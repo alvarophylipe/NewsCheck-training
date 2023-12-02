2ede0a860e215a9b2d38e678ac734c268c6c9e40
Alvaro Phylipe <aphylipe44@gmail.com>
2023-12-02 14:41:50 -0300

    Setup Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='fake-news-inspector',
+    version='0.0.1',
+    author='alvarophylipe',
+    author_email='alvarophylipe@gmail.com'
+    packages=find_packages(),
+    install_requires=[],
+)
