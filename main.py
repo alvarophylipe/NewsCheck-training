fe823afa5499adedc57180cba46756a413ae2508
Alvaro Phylipe <aphylipe44@gmail.com>
2024-02-15 20:21:18 -0300

    Update Vercel Configs #3

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from os import getenv
 
 if __name__ == "__main__":
     sys.path.append("server")
-    port = int(getenv("PORT", 10000))
+    port = int(getenv("PORT", 8000))
     uvicorn.run("server.app:app", host="0.0.0.0", port=port, reload=True)
